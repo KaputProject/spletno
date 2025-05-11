c3defb6ea3088b31c623830a70b534f4c084109a
enejkr <enej.kramar@student.um.si>
2025-05-11 12:39:14 +0200

    SCRUM-8 mongoDB connection mini test todo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import TodoForm from './TodoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+  useEffect(() => {
+        axios.get('http://localhost:5000/todos')
+            .then(response => setTodos(response.data))
+            .catch(error => console.error(error));
+      },
 
+      []);
+  return (<div>
+    <h1>MERN Stack Todo App</h1>
+    <TodoForm onAdd={addTodo}/>
+    <ul>
+      {todos.map((todo) => (<li key={todo._id}>{todo.task}</li>))}
+    </ul>
+  </div>);
+};
 export default App;
