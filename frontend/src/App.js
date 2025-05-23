9d4461a1d78211f0ae58d55958a8a3d1db15f067
kuderluka <luka.kuder@gmail.com>
2025-05-23 13:57:15 +0200

    SCRUM-16: Project set up with latest depelndencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import TodoForm from './TodoForm';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [todos, setTodos] = useState([]);
-  const addTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
-  };
-  useEffect(() => {
-        axios.get('http://localhost:5000/todos')
-            .then(response => setTodos(response.data))
-            .catch(error => console.error(error));
-      },
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-      []);
-  return (<div>
-    <h1>MERN Stack Todo App</h1>
-    <TodoForm onAdd={addTodo}/>
-    <ul>
-      {todos.map((todo) => (<li key={todo._id}>{todo.task}</li>))}
-    </ul>
-  </div>);
-};
 export default App;
